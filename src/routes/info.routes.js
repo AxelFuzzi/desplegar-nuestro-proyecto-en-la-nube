e83cce8bf53dba07be6e29e968c3ea3475eb7738
Axel Fuzzi <axelfuzziii@gmail.com>
2023-03-27 11:49:11 -0300

    fix route dev

--- src/routes/info.routes.js
+++ src/routes/info.routes.js
@@ -32,22 +32,9 @@ router.get('/info', compression(), (req, res) => {
 
 //--------------------------Evaluación de rendimiento------------------------------
 
-router.get('/info/env', compression(), (req, res) => {
-  const args = process.argv.slice(2).join('; ');
-  const info = {
-    args, //Argumentos de entrada
-    path: process.execPath, //Path de ejecución
-    os: process.platform, //Nombre de la plataforma (sistema operativo)
-    pid: process.pid, //Process id
-    nodeVersion: process.version, //Versión de node.js
-    dirPath: process.cwd(), //Carpeta del proyecto
-    memoryUsage: process.memoryUsage.rss() / 2 ** 20, //Memoria total reservada (rss) en MiB
-    numCPUs: cpus().length,
-  };
+router.get('/info/dev', compression(), (req, res) => {
   
-  res.status(200).send(`solicitando el entorno ${process.env.DEV}`, {
-    info,
-  });
+  res.status(200).send(`solicitando el entorno ${process.env.DEV}`);
 });
 
 
